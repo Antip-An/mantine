b469aa8ad168993a8dd615c259e8f8a92f8dd975
Antip-An <anechka-antipina@mail.ru>
2023-03-29 23:37:46 +0300

    new

--- src/components/liner.tsx
+++ src/components/liner.tsx
@@ -1,24 +1,30 @@
-import { createStyles, Header, Menu, Group, Center, Burger, Container } from '@mantine/core';
+import { createStyles, Header, Group, Burger } from '@mantine/core';
+import { rem } from '@mantine/core';
+
 import { useDisclosure } from '@mantine/hooks';
-import { IconChevronDown } from '@tabler/icons';
 import { MantineLogo } from '@mantine/ds';
 
 const useStyles = createStyles((theme) => ({
+  header: {
+    paddingLeft: theme.spacing.md,
+    paddingRight: theme.spacing.md,
+  },
+
   inner: {
-    height: 56,
+    height: rem(56),
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
   },
 
   links: {
-    [theme.fn.smallerThan('sm')]: {
+    [theme.fn.smallerThan('md')]: {
       display: 'none',
     },
   },
 
-  burger: {
-    [theme.fn.largerThan('sm')]: {
+  search: {
+    [theme.fn.smallerThan('xs')]: {
       display: 'none',
     },
   },
@@ -26,7 +32,7 @@ const useStyles = createStyles((theme) => ({
   link: {
     display: 'block',
     lineHeight: 1,
-    padding: '8px 12px',
+    padding: `${rem(8)} ${rem(12)}`,
     borderRadius: theme.radius.sm,
     textDecoration: 'none',
     color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
@@ -37,68 +43,42 @@ const useStyles = createStyles((theme) => ({
       backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
     },
   },
-
-  linkLabel: {
-    marginRight: 5,
-  },
 }));
 
 interface HeaderSearchProps {
-  links: { link: string; label: string; links: { link: string; label: string }[] }[];
+  links: { link: string; label: string }[];
 }
 
 export default function Liner({ links }: HeaderSearchProps) {
   const [opened, { toggle }] = useDisclosure(false);
   const { classes } = useStyles();
 
-  const items = links.map((link) => {
-    const menuItems = link.links?.map((item) => (
-      <Menu.Item key={item.link}>{item.label}</Menu.Item>
-    ));
-
-    if (menuItems) {
-      return (
-        <Menu key={link.label} trigger="hover" exitTransitionDuration={0}>
-          <Menu.Target>
-            <a
-              href={link.link}
-              className={classes.link}
-              onClick={(event) => event.preventDefault()}
-            >
-              <Center>
-                <span className={classes.linkLabel}>{link.label}</span>
-                <IconChevronDown size={12} stroke={1.5} />
-              </Center>
-            </a>
-          </Menu.Target>
-          <Menu.Dropdown>{menuItems}</Menu.Dropdown>
-        </Menu>
-      );
-    }
-
-    return (
-      <a
-        key={link.label}
-        href={link.link}
-        className={classes.link}
-        onClick={(event) => event.preventDefault()}
-      >
-        {link.label}
-      </a>
-    );
-  });
+  const items = links.map((link) => (
+    <a
+      key={link.label}
+      href={link.link}
+      className={classes.link}
+      onClick={(event) => event.preventDefault()}
+    >
+      {link.label}
+    </a>
+  ));
 
   return (
-    <Header height={56} mb={120}>
-      <Container>
-        <div className={classes.inner}>
+    <Header height={56} className={classes.header} mb={120}>
+      <div className={classes.inner}>
+        <Group>
+          <Burger opened={opened} onClick={toggle} size="sm" />
           <MantineLogo size={28} />
-          <Group spacing={5} className={classes.links}>
+        </Group>
+
+        <Group>
+          <Group ml={50} spacing={5} className={classes.links}>
             {items}
           </Group>
-          <Burger opened={opened} onClick={toggle} className={classes.burger} size="sm" />
-        </div>
-      </Container>
+          
+        </Group>
+      </div>
     </Header>
   );
 }
